98f14b98ab12f4e1122c468cc01a94894d1ced49
maged909 <mkingm123@gmail.com>
2022-04-22 20:59:08 +0200

    first

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+(function ($) {
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  $("#my-pic").attr('src',`images/undraw/MyPic${randomIntFromInterval(1, 2)}.png`)
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+        if($('#footer-logo').attr('src') == "images/undraw/Maged1.svg"){
+          $('#footer-logo').attr('src','images/undraw/Maged.svg')
+        }else{
+          $('#footer-logo').attr('src','images/undraw/Maged1.svg')
+        }
+        
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
