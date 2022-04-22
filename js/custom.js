12c998c7059e343b468252069e83d9382728ef36
maged909 <72047025+maged909@users.noreply.github.com>
2022-04-23 00:20:06 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,6 @@
   function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-
   $("#my-pic").attr('src',`images/undraw/MyPic${randomIntFromInterval(1, 2)}.png`)
 
   "use strict";
